12b9afc321c1b46aaad862a1d91633dba6b69fa4
Mark Tatu <a231453mt@rvt.lv>
2025-02-26 21:51:15 +0200

    add validation

--- src/php/login.php
+++ src/php/login.php
@@ -1,39 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/styles.css">
-</head>
-<body>
-    <header></header>
-    <main>
-        <div class="login-cont">
-            <h1>Log in</h1>
-            <form action="login.php" method="post" class="log-form">
-                <input type="text" name="name" placeholder="username or email" class="userNameInput">
-                <input type="password" name="password" placeholder="password" class="passwordInput">
-                <input type="submit" value="login">
-            </form>
-            <a href="register.php">register</a>
-        </div>
-    </main>
-    <?php
+<?php
+    $usernameError = "";
+    $passwordError = "";
 
     $host = 'localhost';
     $dbname = 'postgres';
     $dbuser = 'postgres';
     $dbpassword = '1234';
 
-    $username = $_POST['name'];
-    $password = $_POST['password'];
-    
-    if (empty($username) || empty($password)) {
-        echo "All fields are required.";
-    } else {
-        try {
+    if (isset($_POST['submit'])) {
+        $username = $_POST['name'];
+        $password = $_POST['password'];
+        
+        if (empty($username)) {
+            $usernameError = "field are required.";
+        } else {
             $username = trim(strtolower($username));
+            $username = htmlspecialchars($username);
+
+            if (!preg_match("/^[a-zA-Z0-9@._-]+$/",$username)) {
+                $usernameError = "Name should contain only letters and no spaces.";
+            }
+        }
+
+        if (empty($password)) {
+            $passwordError = "field are required.";
+        } else {
+            $password = trim($password);
+            $password = htmlspecialchars($password);
+
+            if (preg_match("/\s/", $password)) {
+                $passwordError = "password contains spaces";
+            }
+        }
+
+        if (empty($usernameError) && empty($passwordError)) {
             $pdo = new PDO("pgsql:host=$host; dbname=$dbname", $dbuser, $dbpassword);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -52,10 +52,32 @@
                 $_SESSION['username'] = $user['username'];
                 header('Location: /log-reg-form/src/php/main.php');
             }
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
+        }
     }
-}
-    ?>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="login-cont">
+            <h1>Log in</h1>
+            <form action="login.php" method="post" class="log-form">
+                <input type="text" name="name" placeholder="username or email" class="userNameInput">
+                <span><?php echo $usernameError?></span>
+                <input type="password" name="password" placeholder="password" class="passwordInput">
+                <span><?php echo $passwordError?></span>
+                <input type="submit" name="submit" value="login">
+            </form>
+            <a href="register.php">register</a>
+        </div>
+    </main>
 </body>
 </html>
--- src/php/register.php
+++ src/php/register.php
@@ -1,3 +1,80 @@
+<?php
+    $usernameError = "";
+    $emailError = "";
+    $passwordError = "";
+    $password2Error = "";
+
+    $host = 'localhost';
+    $dbname = 'postgres';
+    $dbuser = 'postgres';
+    $dbpassword = '1234';
+
+    if (isset($_POST['submit'])) {
+
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+
+        if (empty($username)) {
+            $usernameError = "field are required.";
+        } else {
+            $username = trim(strtolower($username));
+            $username = htmlspecialchars($username);
+            
+            if (!preg_match("/^[a-zA-Z0-9]+$/",$username)) {
+                $usernameError = "Name should contain only letters and no spaces.";
+            } elseif (strlen($username) < 3 || strlen($username) > 20) {
+                $usernameError = "Name should be between 3 and 20 characters.";
+            }
+        }
+
+        if(empty($email)) {
+            $emailError = "field are required.";
+        } else {
+            $email = trim(strtolower($email));
+            $email = htmlspecialchars($email);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailError = "Invalid email format.";
+            } elseif (strlen($email) > 100) {
+                $emailError = "Email is so long";
+            }
+        }
+
+        if(empty($password)) {
+            $passwordError = "field are required.";
+        } else {
+            $password = trim($password);
+            if (preg_match("/\s/", $password)) {
+                $passwordError = "password contains spaces";
+            } else if (strlen($password) < 8) {
+                $passwordError = "password is short";
+            }
+        }
+        if(empty($password2)) {
+            $password2Error = "field are required.";
+        } else {
+            if ($password != $password2) {
+                $password2Error = "passwords do not match";
+            }
+        }
+
+        if (empty($usernameError) && empty($emailError) && empty($passwordError) && empty($password2Error)) {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            
+            $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpassword);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+            $stmt->execute(['username' => $username,'email' => $email, 'password' => $password]);
+
+            session_start();
+            $_SESSION['username'] = $username;
+            header('Location: /log-reg-form/src/php/main.php');
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,47 +89,18 @@
             <h1>Register</h1>
             <form action="register.php" method="post" class="log-form">
                 <input type="text" name="username" placeholder="username" class="userNameInput">
+                <span><?php echo $usernameError ?></span>
                 <input type="email" name="email" placeholder="email" class="emailNameInput">
+                <span><?php echo $emailError ?></span>
                 <input type="password" name="password" placeholder="password" class="passwordInput">
+                <span><?php echo $passwordError ?></span>
                 <input type="password" name="password2" placeholder="password2" class="passwordInput">
-                <input type="submit" value="register">
+                <span><?php echo $password2Error ?></span>
+                <input type="submit" name="submit" value="register">
                 <p></p>
             </form>
             <a href="../../index.php">log in</a>
         </div>
     </main>
-    <?php
-        $host = 'localhost';
-        $dbname = 'postgres';
-        $dbuser = 'postgres';
-        $dbpassword = '1234';
-
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password2 = $_POST['password2'];
-
-        if (empty($username) || empty($email) || empty($password) || empty($password2)) {
-            echo "All fields are required.";
-        } else {
-            if($password == $password2) {
-            
-                $password = password_hash($password, PASSWORD_DEFAULT);
-                $username = trim(strtolower($username));
-                $email = trim(strtolower($email));
-                $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpassword);
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
-                $stmt->execute(['username' => $username,'email' => $email, 'password' => $password]);
-
-                session_start();
-                $_SESSION['username'] = $username;
-                header('Location: /log-reg-form/src/php/main.php');
-            } else {
-                echo "incorrect password";
-            }
-        }
-    ?>
 </body>
 </html>
